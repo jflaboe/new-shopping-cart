858a73ff682328cdef967116d0eee4693da28a60
John Laboe <johncc97@gmail.com>
2020-05-27 14:21:23 -0500

    static shopping cart

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React, { useEffect, useState } from 'react';
+import { Container, Button } from '@material-ui/core';
 import ProductCardList from './ProductCardList';
+import ShoppingCart from './ShoppingCart';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 const App = () => {
   const [data, setData] = useState({});
+  const [shoppingCartOpen, setShoppingCartOpen] = useState(true);
+  var [shoppingCartItems, setShoppingCartItems] = useState([]);
+  function addItem(item) {
+    for (var i = 0; i < shoppingCartItems.length; i++) {
+      if (item.sku === shoppingCartItems[i] && item.size === shoppingCartItems[i].size) {
+        shoppingCartItems[i].quantity = shoppingCartItems[i].quantity + 1;
+        setShoppingCartItems(shoppingCartItems);
+        return 
+      }
+
+      shoppingCartItems.push(item);
+      setShoppingCartItems(shoppingCartItems);
+
+    }
+  }
   const products = Object.values(data);
   useEffect(() => {
     const fetchProducts = async () => {
@@ -14,7 +32,15 @@ const App = () => {
   }, []);
 
   return (
-    <ProductCardList data={products} />
+    <Container>
+      
+      <ProductCardList data={products} />
+      <ShoppingCart items={[]} isOpen={shoppingCartOpen} setIsOpen={setShoppingCartOpen}/>
+      <Button onClick={()=>{setShoppingCartOpen(true)}} style={{position: 'absolute', top: 0, 'right': 0}}>
+        <ShoppingCartIcon />
+      </Button>
+    </Container>
+    
   );
 };
 
---
+++ src/ShoppingCart.js
@@ -0,0 +1,80 @@
+import React, {useState, useRef, useEffect} from 'react'
+import { Drawer, Grid, Typography, Button, Container } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+import useWindowDimensions from './useWindowDimensions'
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
+
+function ShoppingCartItem(props){
+    const item = props.item;
+    
+    const [imgSize, setImgSize] = useState(19);
+    
+
+
+    return (
+        <Grid container direction="row" spacing={3}>
+            <Grid item xs={2}>
+                <img src={"./data/products/" + item.sku.toString() + '_2.jpg'} style={{width: imgSize}}/>
+            </Grid>
+            <Grid item xs={8}>
+                <Typography>{item.title}</Typography>
+                <Typography>{item.size}|{item.description}</Typography>
+                <Typography>Quantity: {item.quantity}</Typography>
+            </Grid>
+            <Grid item>
+                <Button align={'right'}><CloseIcon/></Button>
+            </Grid>
+        </Grid>
+    )
+}
+
+export default function ShoppingCart(props) {
+    const items = props.items;
+    const { height, width } = useWindowDimensions();
+    const delItem = props.delItem;
+    const increaseItem = props.increaseItem;
+    const isOpen = props.isOpen;
+    const setIsOpen = props.setIsOpen;
+    var totalCost = 0;
+    for (var i =0; i < items.length; i++) {
+        totalCost += items[i].price * items[i].quantity;
+    }
+
+    return (
+        <React.Fragment>
+        <Drawer variant='persistent' anchor="right" open={isOpen} style={{width: '30%'}}>
+            <Grid container direction="column" style={{width: 400}}>
+            <Grid item>
+                <Container style={{height: height * 0.11, borderBottomStyle: 'solid'}}>
+                    <Button onClick={() => {setIsOpen(false);}}><CloseIcon /></Button>
+                    
+                    <Typography style={{textAlign: "center", width: '100%'}}>Shopping Cart</Typography>
+                </Container>
+            </Grid>
+            <Grid item>
+            <Container style={{height: height * 0.65}}>
+                {items.map((item, index) => {
+                    return (
+                        <ShoppingCartItem key={index} item={item} delItem={delItem} />
+                    );
+                })
+                }
+            </Container>
+                
+            </Grid>
+            <Grid item>
+                <Grid container direction="row">
+                    <Grid item xs={6}>
+                        <Typography style={{textAlign: "left", paddingLeft: 20}}>Subtotal:</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography style={{textAlign: "right", paddingRight: 20}}>${totalCost.toFixed(2)}</Typography>
+                    </Grid>
+                </Grid>
+            </Grid>
+            </Grid>
+        </Drawer></React.Fragment>
+    )
+
+}
